d551b4b0f143e4a735ca9e5d2932aade4bc1e8ff
Xu Sheng Jie <xushengjie@oneapm.com>
2016-12-17 15:53:48 +0800

    xushengjie

--- demo10/src/main/java/com/testin/App.java
+++ demo10/src/main/java/com/testin/App.java
@@ -1,11 +1,14 @@
 package com.testin;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testin.domain.DruidDataSource;
 import com.testin.mapper.DruidDataSourceMapper;
@@ -19,13 +22,17 @@ public class App {
     @Autowired
     private DruidDataSourceMapper druidDataSourceMapper;
     
-    public void test() throws JsonProcessingException {
+    @SuppressWarnings("unchecked")
+    public void test() throws IOException {
         DruidDataSource record = druidDataSourceMapper.selectByPrimaryKey("payfee");
         System.out.println(new String(record.getCommitMetadataPayload()));
         System.out.println(mapper.writeValueAsString(record));
+        Map<String, Object> m = mapper.readValue(new String(record.getCommitMetadataPayload()),
+                                                 Map.class);
+        System.out.println(Objects.toString(m));
     }
     
-    public static void main(String[] args) throws InterruptedException, JsonProcessingException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(App.class,
                                                                                   args);
         App app = applicationContext.getBean(App.class);
